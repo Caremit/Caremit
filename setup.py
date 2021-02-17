35732ab4973669740d65597fa66e68474ebe774b
Stephan Sahm <stephan.sahm@gmx.de>
2021-02-17 15:04:52 +0100

    restructured setup.py to be top-level and include requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, find_packages
 
+subfolder = "src/python"
 setup(name='caremit',
       version='0.1.0',
       description='Python package for factoring out helpers which we reused in our caremit project.',
       author='Stephan Sahm, Moriz Münst, Marcel Blistein',
-      author_email='stephan.sahm@gmx.de, moses-github@geekbox.com, marcel.blistein@gmail.com'
-      packages=find_packages(),
+      author_email='stephan.sahm@gmx.de, moses-github@geekbox.com, marcel.blistein@gmail.com',
+      packages=find_packages(subfolder),
+      package_dir={"": subfolder},
       install_requires=[
             'toolz',
+            'wfdb',
             'pandas',
             'numpy',
+            'scipy',
+            'keras',
+            'sklearn',
+            'jedi==0.17.0',
+      ])
